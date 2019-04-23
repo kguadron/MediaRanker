ab32cb92ec8b5e41e41c16358fd8e099825d34ed
Karla Guadron <ksguadron@gmail.com>
2019-04-22 21:38:32 -0700

    Wrote index method and view for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,13 @@
 class WorksController < ApplicationController
+
+  def index
+    def index
+      @works = Work.all
+      @media_spotlight = @works.sample
+    end
+  end
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
