0842e0583f5f0b0afbe7c48069193b510b552110
Karla Guadron <ksguadron@gmail.com>
2019-04-22 20:31:45 -0700

    Generated work model and controller. Parsed CSV and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'media_seeds.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Work.new
+  t.category = row['category']
+  t.title = row['title']
+  t.creator = row['creator']
+  t.publication_year = row['publication_year']
+  t.description = row['description']
+  t.save
+  puts "#{t.title} saved"
+end
