87d17a3135139cbdd82658e840f7bb7f68e5c1f7
Karla Guadron <ksguadron@gmail.com>
2019-04-23 15:16:04 -0700

    Created index method and view for work model

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,19 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+    @media_spotlight = @works.sample
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+    unless @work
+      head :not_found
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
