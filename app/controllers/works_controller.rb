0e23b66d07c077b83546bb0aba80e626e9e4c7a2
Karla Guadron <ksguadron@gmail.com>
2019-04-24 13:10:45 -0700

    Updated views with create form and a homepage

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,13 +1,30 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
-    @media_spotlight = @works.sample
   end
 
   def show
     @work = Work.find_by(id: params[:id])
     unless @work
       head :not_found
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    is_successful = @work.save
+
+    if is_successful
+      flash[:success] = "Work added successfully"
+      redirect_to work_path(@work.id)
+    else
+      flash[:error] = "Work could not be added. Try again."
+      render :new
     end
   end
 
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,6 @@
 class Work < ApplicationRecord
+  validates :title, presence: true
+  validates :creator, presence: true
+  validates :title, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true }
 end
