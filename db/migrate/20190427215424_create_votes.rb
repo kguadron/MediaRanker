4a4acc0a7bc4ff223909dd7d54504cf39476092f
Karla Guadron <ksguadron@gmail.com>
2019-04-27 15:12:45 -0700

    Created vote model and controller

---
+++ db/migrate/20190427215424_create_votes.rb
@@ -0,0 +1,9 @@
+class CreateVotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :votes do |t|
+      t.belongs_to :work, index: true
+      t.belongs_to :user, index: true
+      t.timestamps
+    end
+  end
+end
