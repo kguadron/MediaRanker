63a1d30aa0c47cdfb7e832044c43ff9a3035b3a4
Karla Guadron <ksguadron@gmail.com>
2019-04-27 14:40:29 -0700

    Wrote user validation and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,18 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(username: "new_user") }
 
-  it "must be valid" do
+  it "must be valid with good data" do
     value(user).must_be :valid?
+  end
+
+  it "fails without a username" do
+    user.username = nil
+
+    result = user.valid?
+
+    expect(result).must_equal false
+    expect(user.errors.messages).must_include :username
   end
 end
